4caa3c28aa24ab5e31353e9857503297e3258535
Kris Foss <kris.foss@gmail.com>
2016-09-19 18:06:27 -0600

    initial commit

---
+++ test/test-index.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const ScoreWord = require('../lib/components/ScoreWord.js');
+
+describe('the test bundle', function () {
+  it('isnt a garbage fire, yet', function () {
+    assert(true);
+  });
+});
